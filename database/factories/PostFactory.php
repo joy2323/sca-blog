59adf1dd955d5e11673adf88188e74f242f1bbe6
joy2323 <joyjoel2011@gmail.com>
2023-05-20 04:30:40 +0100

    create post seeder, factory, migrations and controller

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => faker()->sentence, //Generates a fake sentence
+            'body' => faker()->paragraph(), //generates fake 30 paragraphs
+            'user_id' => User::factory() //Generates a User from factory and extracts id
+
+        ];
+    }
+}
---
+++ database/migrations/2023_05_20_031605_create_posts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->text('title');  // Title of our blog post          
+            $table->text('body');   // Body of our blog post
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();     // user_id of our blog post author          
+            // $table->text('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
