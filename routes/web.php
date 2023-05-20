69c08b833552d58a771fe4ca8a1d96b1cc21d7cb
joy2323 <joyjoel2011@gmail.com>
2023-05-20 05:50:13 +0100

    update

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// The route we have created to show all blog posts.
+Route::get('/blog', [\App\Http\Controllers\PostController::class, 'index']);
+Route::get('/blog/{blogPost}', [\App\Http\Controllers\PostController::class, 'show']);
+
+
+Route::get('/blog/create/post', [\App\Http\Controllers\PostController::class, 'create']); //shows create post form
+Route::post('/blog/create/post', [\App\Http\Controllers\PostController::class, 'store']); //saves the created post to the databse
+Route::get('/blog/{blogPost}/edit', [\App\Http\Controllers\PostController::class, 'edit']); //shows edit post form
+Route::put('/blog/{blogPost}/edit', [\App\Http\Controllers\PostController::class, 'update']); //commits edited post to the database 
+Route::delete('/blog/{blogPost}', [\App\Http\Controllers\PostController::class, 'destroy']); //deletes post from the database
+
+
+
