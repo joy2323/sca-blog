493e8ec0945340947a0def1a9161c9914fe85171
joy2323 <joyjoel2011@gmail.com>
2023-05-21 22:28:27 +0100

    update env

--- config/database.php
+++ config/database.php
@@ -45,12 +45,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            // 'url' => env('DATABASE_URL'),
-            'host' => 'sca-blog-server.mysql.database.azure.com',
-            'port' => '3306',
-            'database' => 'sca-blog-database',
-            'username' => 'ihvflbboij',
-            'password' =>  '7C5265BPJSRN7YE7$',
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', 'localhost'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
